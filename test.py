3c8debbc221bfdad74c32b4d7d69035e334c65c0
x89 <napalm10@gmail.com>
2015-03-16 16:04:52 +0000

    Inherited templates.

--- test.py
+++ test.py
@@ -11,32 +11,23 @@ app.secret_key = 'pawkette'
 def index():
     return render_template('index.html')
 
-@app.route("/foo/<int:foo>")
-def bar(foo):
-    if foo == 5:
-        return url_for('static', filename="style.css")
-    else:
-        return str(foo * 2)
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == 'POST':
         session['user'] = request.form['username']
+        flash('Hi %s!' % session['user'])
         return redirect(url_for('index'))
     else:
         if session.get('user'):
-            return "Already logged in as %s." % session['user']
+            flash("You've already logged in as %s" % session['user'])
+            return redirect(url_for('index'))
         return render_template('login.html')
 
 @app.route('/logout')
 def logout():
     session.pop('user', None)
-    flash('You have logged out.')
+    flash('You have logged out')
     return redirect(url_for('index'))
-
-@app.route("/user/<username>")
-def user(username):
-    return render_template("user.html", user=username)
 
 if __name__ == "__main__":
     app.debug = True
